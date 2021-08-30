d7ffb2b39acc22737ad68d655d3c42c20c7ce585
dani-mashasha <dmmashasha@gmail.com>
2021-08-30 20:38:56 +0300

    connected to mongo atlas

--- server.js
+++ server.js
@@ -1,134 +1,143 @@
 const express = require("express");
 const fs = require("fs");
-const { v4: uuidv4 } = require('uuid');
-const { query } = require("express");
+const { v4: uuidv4 } = require("uuid");
 
-const mongoose = require('mongoose');
-const { json } = require("body-parser");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 
 const productSchema = new mongoose.Schema({
-    title: String,
-    price: Number,
-    description: String,
-    category: String,
-    image: String,
-
+  title: String,
+  price: Number,
+  description: String,
+  category: String,
+  image: String
 });
 
-const Product = mongoose.model('Product', productSchema);
-
+const Product = mongoose.model("Product", productSchema);
 
 app.get("/", (req, res) => {
-    res.send("Home");
+  res.send("Home");
 });
 
-
-app.get("/products",async (req, res) => {
-    const {category, min, max} = req.query;
-    await Product.find((err, products) => {
-        if(category){
-            category === "all items"?  products = products: products =products.filter(product=>product.category === category);
-        }
-        if(min){
-            products = products.filter(product => product.price >= +min);
-        }
-        if(max){
-            products = products.filter(product => product.price <= +max);
-        }
-        res.send(products);
-    })
+app.get("/products", (req, res) => {
+  const { category, min, max } = req.query;
+  Product.find().then((products) => {
+    if (category) {
+      category === "all items"
+        ? (products = products)
+        : (products = products.filter(
+            (product) => product.category === category
+          ));
+    }
+    if (min) {
+      products = products.filter((product) => product.price >= +min);
+    }
+    if (max) {
+      products = products.filter((product) => product.price <= +max);
+    }
+    res.send(products);
+  });
 });
 
-
-app.get("/products/:id", async(req, res) => {
-    const {id} = req.params;
-    await Product.findById(id, (err, product) => {
-        if(err){
-            console.log(err)
-        }
-        console.log(product);
-        res.send(product);
-    })
-
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  Product.findById(id).then(
+    (product) => {
+      res.send(product);
+    },
+    (err) => {
+      res.send("Cant Find Product");
+    }
+  );
 });
 
-
-app.post("/products", async(req, res) => {
-    const {title, price, description, category, image} = req.body;
+app.post("/products", (req, res) => {
+  const { title, price, description, category, image } = req.body;
+  if (title && price && description && category && image) {
     const product = new Product({
-        title,
-        price,
-        description,
-        category,
-        image
+      title,
+      price,
+      description,
+      category,
+      image,
     });
-    try{
-        await product.save();
-        res.status(201).send(product);
-    } catch(error){
-        res.status(500).send(error);
+    product.save().then(() => res.send(product));
+  } else {
+    res.send("Please fill in al the parameters");
+  }
+});
+
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, price, description, category, image } = req.body;
+
+  const updatedProduct = {};
+  title ? (updatedProduct.title = title) : null;
+  price ? (updatedProduct.price = price) : null;
+  description ? (updatedProduct.description = description) : null;
+  category ? (updatedProduct.category = category) : null;
+  image ? (updatedProduct.image = image) : null;
+
+  Product.findByIdAndUpdate(id, updatedProduct, {new : true}).then(
+    (product) => {
+      res.send(product);
+    },
+    (err) => {
+      res.send("Product Not Found");
     }
+  );
 });
 
-
-app.put("/products/:id", async(req, res) => {
-    const {id} = req.params;
-    const {title, price, description, category, image} = req.body;
-
-    const updatedProduct = {};
-    title? updatedProduct.title = title : null;
-    price? updatedProduct.price = price : null;
-    description? updatedProduct.description = description : null;
-    category? updatedProduct.category = category : null;
-    image? updatedProduct.image = image : null;
-
-    await Product.findByIdAndUpdate(id, updatedProduct, (err, product) => {
-        res.send(product)
-    })
-});
-
-
-app.delete("/products/:id",async (req, res) => {
-    try{
-        const {id} = req.params;
-        await Product.findByIdAndDelete(id, (err, product) => {
-        res.send(product)})
-    } catch(err) {
-        res.send("Product Not Found")
+app.delete("/products/:id", (req, res) => {
+  const { id } = req.params;
+  Product.findByIdAndDelete(id).then(
+    (product) => {
+      res.send(product);
+    },
+    (err) => {
+      res.send("Product Not Found");
     }
-    
+  );
 });
 
-
-function initProducts(){
-    Product.findOne((err, product) => {
-        if(!product){
-            fs.readFile("./initialProducts.json", "utf8", (err, data) => {
-                let initialProducts = JSON.parse(data);
-                Product.insertMany(initialProducts, (err, products) => {
-                    console.log(products)
-                })
-            })
-        }
+function initProducts() {
+  Product.findOne()
+    .then((product) => {
+      if (!product) {
+        fs.readFile("./initialProducts.json", "utf8", (err, data) => {
+          let initialProducts = JSON.parse(data);
+          Product.insertMany(initialProducts).then((products) => {
+            console.log(products);
+          });
+        });
+      } else {
+        console.log("BD Is Full");
+      }
     })
-};
+    .catch((err) => {
+      console.log("ERR:", err);
+    });
+}
 
 initProducts();
 
-
-mongoose.connect(
-    'mongodb://localhost/gocode_shop', 
-    {  useNewUrlParser: true,
-       useCreateIndex: true,
-       useUnifiedTopology: true},
-       () => {
-           console.log("Connected To DB")
-           app.listen("8080");  
-       }
+mongoose
+  .connect(
+      "mongodb+srv://dani:dm8036226@cluster0.z9oum.mongodb.net/gocode_shop?retryWrites=true&w=majority", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      app.listen("8080");
+    },
+    (err) => {
+      console.log(err);
+    }
   );
-  
-
